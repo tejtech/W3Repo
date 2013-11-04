2b47360a1da2bb4bc2a9fb39f31bb843610c059e
wm <wakkir@yahoo.com>
2013-11-04 12:31:31 +0000

    added builder pattern

---
+++ Workspaces/intelij/home/study/StudyBase/src/main/java/com/wakkir/designpattern/creational/builder/BuilderPatternDemo.java
@@ -0,0 +1,24 @@
+package com.wakkir.designpattern.creational.builder;
+
+/**
+ * User: wakkir.muzammil
+ * Date: 04/11/13
+ * Time: 11:04
+ */
+public class BuilderPatternDemo
+{
+    public static void main(String[] args)
+    {
+        MealBuilder mealBuilder = new MealBuilder();
+
+        Meal vegMeal = mealBuilder.prepareVegMeal();
+        System.out.println("Veg Meal");
+        vegMeal.showItems();
+        System.out.println("Total Cost: " + vegMeal.getCost());
+
+        Meal nonVegMeal = mealBuilder.prepareNonVegMeal();
+        System.out.println("\n\nNon-Veg Meal");
+        nonVegMeal.showItems();
+        System.out.println("Total Cost: " + nonVegMeal.getCost());
+    }
+}
---
+++ Workspaces/intelij/libs/install-jars.bat
@@ -0,0 +1,9 @@
+call mvn install:install-file -Dfile=json-lib-2.4.jar -DgroupId=net.sf.json -DartifactId=json-lib -Dversion=2.4 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=ehcache-1.2.3.jar -DgroupId=net.sf.ehcache -DartifactId=ehcache -Dversion=1.2.3 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=ezmorph-1.0.6.jar -DgroupId=net.sf.ezmorph -DartifactId=ezmorph -Dversion=1.0.6 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=metadata-extractor-2.6.4.jar -DgroupId=com.drew.imaging -DartifactId=metadata-extractor -Dversion=2.6.4 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=xmpcore-1.0.jar -DgroupId=com.adobe.xmp -DartifactId=xmpcore -Dversion=1.0 -Dpackaging=jar
